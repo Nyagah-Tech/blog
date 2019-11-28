b96b47a4c19a013a57a9d4737e7ae9f57733ffa3
dan <dan@localhost.localdomain>
2019-11-28 11:45:28 +0300

    create user app that helps us create a new user

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
-from flsk_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()

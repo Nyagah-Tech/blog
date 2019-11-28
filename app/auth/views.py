b994bb87f98648191f3b58a292a5755651bbc7ef
dan <dan@localhost.localdomain>
2019-11-28 15:36:48 +0300

    crate a method on submit of registration form a new user is made

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,21 @@
-from flask import render_template,redirect
+from flask import render_template,redirect,url_for
 from . import auth
+from .. import User
+from .. import db
+from .forms import RegForm,LoginForm 
 
 @auth.route('/login')
 def login():
     return render_template('auth/login.html' , form = loginform)
+
+@auth .route('/registration')
+def registration():
+    form =RegForm
+    if form.validate_on_submit():
+        user = User(email = form.email.data,username = form.username.data,password = form.password.data)
+
+        db.session.add(user)
+        db.commit()
+        return redirect(url_for('auth.login'))
+        title = 'registration'
+    return render_template('auth/registration.html', regform =form)

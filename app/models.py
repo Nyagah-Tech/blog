969329e5b1d97d1eedc52bb6d1d40e6cc7f5fbe6
dan <dan@localhost.localdomain>
2019-11-28 12:22:58 +0300

    create a function that raises an error if one wants to read the password

--- app/models.py
+++ app/models.py
@@ -1,9 +1,17 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(30))
+    pass_code = db.Column(db.String(16))
+
+
+    @property
+    def password(self):
+        raise AttributeError('Access denied.Read acess is permitted ')
 
     def __repr__(self):
         return f'User {self.username}'

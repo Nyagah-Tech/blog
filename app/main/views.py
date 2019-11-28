e4f1a30438e3edb818ee384453da2a12ab7f85ed
dan <dan@localhost.localdomain>
2019-11-28 22:37:39 +0300

    create the newblog.html that will display our newblog form

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
 from flask_login import login_required,current_user
 from flask import render_template,redirect,url_for,abort
 from .. import db
-from ..models import User
+from ..models import User,Blog
 from . import main
+from .forms import New_blog_form
 
 @main.route('/')
 def index():
@@ -16,4 +17,15 @@ def new_blog(uname):
     user = User.query.filter_by(username = uname).first()
     if user is None:
         abort(404)
-    form =NewBlog()
+    form = New_blog_form()
+    if form.validate_on _submit():
+        title = form.title.data
+        body = form.title.data
+        
+        new_blog = Blog(blog_title = title,blog_body = body,user = current_user.username)
+
+        new_blog.save_blog()
+        return redirect(url_for('.profile', uname = current_user.username))
+
+    title = 'new pitch'
+    return render_template('new_blog.html',title = title, newBlog_form = form)

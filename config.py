16a863f1021f482e984fe3c48df319505ff5817f
dan <dan@localhost.localdomain>
2019-11-30 10:15:54 +0300

    create a new configuration that specifies the destination where we store our images

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ class Config:
 
     SECRET_KEY ='dm/01254'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dan:12345@localhost/blog'
-
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    
     pass
 
 class ProdConfig(Config):

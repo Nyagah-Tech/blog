e633efbc5acd9172bd3e93692908debf37abff39
dan <dan@localhost.localdomain>
2019-12-01 13:44:41 +0300

    setup smtp server and configure the ports to use gmail smtp server port

--- config.py
+++ config.py
@@ -3,6 +3,12 @@ class Config:
     SECRET_KEY ='dm/01254'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dan:12345@localhost/blog'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'danmuv12@gmail.com'
+    MAIL_PASSWORD = 'Dm17/01254'
     
     pass
 

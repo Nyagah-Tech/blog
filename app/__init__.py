c903245ff563b7c31f004c0a2d2024ae97f4cc81
dan <dan@localhost.localdomain>
2019-12-01 14:30:42 +0300

    import the mail instance from the application factory module

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 
 bootstrap = Bootstrap()
@@ -12,6 +13,7 @@ photos = UploadSet('photos',IMAGES)
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+mail = Mail()
 
 
 def create_app(config_name):
@@ -24,6 +26,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     # configure uploadset
     configure_uploads(app,photos)
